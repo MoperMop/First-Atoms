8209401c0f4466613a7a1f2e775fe139633cc109
Daniel Parker <dantiptoe@gmail.com>
2024-02-20 10:49:04 -0700

    added Helium

---
+++ particles/helium.js
@@ -0,0 +1,54 @@
+import Particle from "./particle.js";
+
+
+export default class Helium extends Particle {
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} [rotation]
+   */
+  constructor(x, y, rotation = 0) {
+    super(x, y);
+
+
+    this.rotation = rotation;
+    this.velocityRotation = 0;
+
+
+    let neutrons = 0;
+
+    while (this.#particles.length < 4) {
+      if (
+        this.#particles.length - neutrons < 2 &&
+        (neutrons >= 2 || Math.random() < 0.5)
+      ) {
+        this.#particles.push("proton");
+
+        continue;
+      }
+      neutrons++;
+      this.#particles.push("neutron");
+    }
+  }
+  /** @type {("proton" | "neutron")[]}*/
+  #particles = [];
+
+
+  draw() {
+    for (let i = 0; i < 4; i++) {
+      const pos = /** @type {const} */ ([
+        Math.sin(i * Math.PI / 2 + this.rotation) * 25 + this.x,
+        Math.cos(i * Math.PI / 2 + this.rotation) * 25 + this.y,
+      ]);
+
+      if (this.#particles[i] === "proton") Particle.drawProton(...pos);
+      else Particle.drawNeutron(...pos);
+    }
+  }
+
+  physics() {
+    super.physics();
+
+    this.rotation += this.velocityRotation * Particle.deltaTime;
+  }
+}
--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 import Particle from "./particles/particle.js";
-import Electron from "./particles/electron.js";
-import Hydrogen from "./particles/hydrogen.js";
 
+import Electron from "./particles/electron.js";
+
+import Hydrogen from "./particles/hydrogen.js";
+import Helium from "./particles/helium.js";
+
+
+const h = new Hydrogen(100, 100);
+h.velocityX = 100;
 
 const e = new Electron(100, 200);
 e.velocityX = 100;
 
-const h = new Hydrogen(100, 100);
-h.velocityX = 100;
+const he = new Helium(100, 300);
+he.velocityX = 100;
+he.velocityRotation = Math.PI;
 
 
 Particle.render();
